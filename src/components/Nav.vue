b221e053a23ccb96c3f96680a445017eb46cbbaa
steven <1938848188@qq.com>
2020-08-13 22:29:18 +0800

    money页面布局

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,10 +1,8 @@
 <template>
-  <div>
-    <div>
-      <router-link to="/money">记账</router-link>|
-      <router-link to="/labels">标签</router-link>|
-      <router-link to="/statistics">统计</router-link>
-    </div>
+  <div class="nav">
+    <router-link to="/money">记账</router-link>|
+    <router-link to="/labels">标签</router-link>|
+    <router-link to="/statistics">统计</router-link>
   </div>
 </template>
 
@@ -15,4 +13,7 @@ export default {
 </script>
 
 <style lang='scss' scoped>
+.nav {
+  border: 1px solid red;
+}
 </style>
